deee0bc095c795181bb08aaef9ba28fb38ee38f7
Devika101 <134152693+Devika101@users.noreply.github.com>
2025-06-24 19:05:51 +0530

    done

--- predict_test.py
+++ predict_test.py
@@ -1,17 +1,16 @@
 import joblib
 import pandas as pd
 
-# Load the saved model
+
 model = joblib.load("rf_model.pkl")
 print("✅ Model loaded")
 
-# Example input: [time, protocol, length]
-# You can adjust these values based on your CSV
+
 test_data = pd.DataFrame([
-    [1750397778.533793, 9, 72]   # Replace with actual values from your CSV
+    [1750397778.533793, 9, 72]   
 ], columns=["time", "protocol", "length"])
 
-# Make prediction
+
 prediction = model.predict(test_data)
-print("🧠 Prediction:", "Attack" if prediction[0] == 1 else "Normal")
+print(" Prediction:", "Attack" if prediction[0] == 1 else "Normal")
 
